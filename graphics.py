714f1410214f4c09ad626bf2b22850220754e601
louashka <louashka@gmail.com>
2022-08-09 23:36:27 +0800

    Update motion planner

---
+++ graphics.py
@@ -0,0 +1,152 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.animation import FuncAnimation
+from matplotlib.patches import Rectangle
+import numpy as np
+import globals_
+
+
+LINK_DIAG = ((globals_.L_LINK / 2)**2 + (globals_.D_BRIDGE / 2)**2)**(1 / 2)
+fig, ax = plt.subplots()
+q_list = []
+q_target = []
+s_array = []
+
+link = Rectangle((0, 0), 0, 0, fc='y')
+arc1, = ax.plot([], [], lw=2, color="blue")
+arc2, = ax.plot([], [], lw=2, color="blue")
+centre, = ax.plot([], [], lw=2, marker=".", color="black")
+
+stiffness_text = ax.text(0, 0, '', fontsize=12)
+
+target_link = Rectangle((0, 0), 0, 0, fc='y', alpha=0.5)
+target_arc1, = ax.plot([], [], lw=1, color="blue", alpha=0.5)
+target_arc2, = ax.plot([], [], lw=1, color="blue", alpha=0.5)
+
+
+def init():
+    global ax
+
+    x_range, y_range = defineRange()
+    ax.set_xlim(x_range)
+    ax.set_ylim(y_range)
+    ax.set_aspect("equal")
+
+    ax.add_patch(link)
+
+    if q_target:
+        ax.add_patch(target_link)
+
+
+def defineRange():
+    margin = 0.1
+
+    q_array = np.array(q_list)
+    x_min, y_min = q_array[:, :2].min(axis=0)
+    x_max, y_max = q_array[:, :2].max(axis=0)
+
+    ax_range = max(x_max - x_min, y_max - y_min) + margin
+
+    x_range = (x_min - margin, x_min + ax_range)
+    y_range = (y_min - margin, y_min + ax_range)
+
+    return x_range, y_range
+
+
+def genArc(q, seg):
+    s = np.linspace(0, globals_.L_VSS, 50)
+
+    flag = -1 if seg == 1 else 1
+
+    gamma_array = q[2] + flag * q[2 + seg] * s
+
+    x_0 = q[0] + flag * np.cos(q[2]) * globals_.L_LINK / 2
+    y_0 = q[1] + flag * np.sin(q[2]) * globals_.L_LINK / 2
+
+    if q[2 + seg] == 0:
+        x = x_0 + [0, flag * globals_.L_VSS * np.cos(q[2])]
+        y = y_0 + [0, flag * globals_.L_VSS * np.sin(q[2])]
+    else:
+        x = x_0 + np.sin(gamma_array) / \
+            q[2 + seg] - np.sin(q[2]) / q[2 + seg]
+        y = y_0 - np.cos(gamma_array) / \
+            q[2 + seg] + np.cos(q[2]) / q[2 + seg]
+
+    return [x, y]
+
+
+def update(i):
+    global link, target_link, arc1, arc2, centre, stiffness_text, target_arc1, target_arc2
+    q = q_list[i]
+
+    x = q[0]
+    y = q[1]
+    phi = q[2]
+
+    x0 = x - globals_.L_LINK / 2
+    y0 = y - globals_.D_BRIDGE / 2
+
+    link.set_width(globals_.L_LINK)
+    link.set_height(globals_.D_BRIDGE)
+    link.set_xy([x0, y0])
+
+    transform = mpl.transforms.Affine2D().rotate_around(
+        x, y, phi) + ax.transData
+    link.set_transform(transform)
+
+    seg1 = genArc(q, 1)
+    seg2 = genArc(q, 2)
+
+    arc1.set_data(seg1[0], seg1[1])
+    arc2.set_data(seg2[0], seg2[1])
+
+    if s_array[i][0] == 0:
+        arc1.set_color("red")
+    else:
+        arc1.set_color("blue")
+
+    if s_array[i][1] == 0:
+        arc2.set_color("red")
+    else:
+        arc2.set_color("blue")
+
+    centre.set_data(x, y)
+
+    stiffness_text.set_text(
+        "s1: " + str(s_array[i][0]) + ", s2: " + str(s_array[i][1]))
+    stiffness_text.set_position((0.05, 0.1))
+
+    if q_target:
+
+        x_t = q_target[0] - globals_.L_LINK / 2
+        y_t = q_target[1] - globals_.D_BRIDGE / 2
+        phi_t = q_target[2]
+
+        target_link.set_width(globals_.L_LINK)
+        target_link.set_height(globals_.D_BRIDGE)
+        target_link.set_xy([x_t, y_t])
+
+        target_transform = mpl.transforms.Affine2D().rotate_around(
+            q_target[0], q_target[1], phi_t) + ax.transData
+        target_link.set_transform(target_transform)
+
+        target_seg1 = genArc(q_target, 1)
+        target_seg2 = genArc(q_target, 2)
+
+        target_arc1.set_data(target_seg1[0], target_seg1[1])
+        target_arc2.set_data(target_seg2[0], target_seg2[1])
+
+        return link, arc1, arc2, centre, stiffness_text, target_link, target_arc1, target_arc2,
+
+    return link, arc1, arc2, centre, stiffness_text,
+
+
+def plotMotion(q, s, frames, q_t=[]):
+    global q_list, s_array, q_target
+    q_list = q
+    s_array = s
+    q_target = q_t
+
+    anim = FuncAnimation(fig, update, frames,
+                         init_func=init, interval=1, repeat=True)
+    plt.show()
--- mainController.py
+++ mainController.py
@@ -1,38 +1,130 @@
 import serial
+import numpy as np
+import kinematics
+import graphics
+import random as rnd
 from pynput import keyboard
 
 
 class Controller:
 
     def __init__(self, portName):
-        self.serial_port = serial.Serial(portName, 115200)
+        # self.serial_port = serial.Serial(portName, 115200)
+        self.serial_port = None
 
-    def sendCommands(self, wheels_v):
+    def motionPlanner(self, q_0, q_target):
+        dt = 0.1  # step size
+        # A set of possible stiffness configurations
+        s = [[0, 0], [0, 1], [1, 0], [1, 1]]
+        # Initialize a sequence of VSB stiffness values
+        s_list = []
+        # 2SRR always starts from the rigid state
+        # s_list.append(s[0])
+        # Initialize the number of stiffness transitions
+        switch_counter = 0
+
+        # Initialize a trajectory
+        q_list = []
+        q = q_0  # current configuration
+        # q_list.append(q)
+
+        # Initialize a sequence of velocity commands
+        v_list = []
+
+        # A set of possible configurations and velocities
+        q_ = [None] * len(s)
+        v_ = [None] * len(s)
+
+        q_t = np.array(q_target)
+        # Euclidean distance between current and target configurations (error)
+        dist = np.linalg.norm(q - q_t)
+
+        t = 0.1  # current time
+        # feedback gain
+        velocity_coeff = np.ones((5,), dtype=int)
+        # Index of the current stiffness configuration
+        current_i = None
+
+        while dist > 0:
+
+            flag = False  # indicates whether VSB stiffness has changed
+
+            # INVERSE KINEMATICS
+
+            q_tilda = velocity_coeff * (q_t - q) * t
+            for i in range(len(s)):
+                # Jacobian matrix
+                J = kinematics.hybridJacobian(q_0, q, s[i])
+                # velocity input commands
+                v_[i] = np.matmul(np.linalg.pinv(J), q_tilda)
+                q_dot = np.matmul(J, v_[i])
+                q_[i] = q + (1 - np.exp(-1 * t)) * q_dot * dt
+
+            # Determine the stiffness configuration that promotes
+            # faster approach to the target
+            dist_ = np.linalg.norm(q_ - q_t, axis=1)
+            min_i = np.argmin(dist_)
+
+            # The extent of the configuration change
+            delta_q_ = np.linalg.norm(q - np.array(q_), axis=1)
+
+            # Stiffness transition is committed only if the previous
+            # stiffness configuration does not promote further motion
+            if min_i != current_i and current_i is not None:
+                if delta_q_[current_i] > 10**(-17):
+                    min_i = current_i
+                else:
+                    flag = True
+
+            current_i = min_i  # update current stiffness
+            q = q_[current_i]  # update current configuration
+            dist = np.linalg.norm(q - q_t)  # update error
+
+            if (delta_q_[current_i] > 10 ** (-5)):
+                q_list.append(q)
+                v_list.append(v_[current_i])
+                s_list.append(s[current_i])
+
+                if flag:
+                    switch_counter += 1
+
+            t += dt  # increment time
+
+        return q_list, v_list, s_list, switch_counter
+
+    def wheelDrive(self, v, s):
+        w = [0, 0, 0, 0]
+        return w
+
+    def sendCommands(self, wheels_v_list, s_list):
+        return
+
+    def sendData(self, q_0, commands):
 
         msg = "s"
 
-        for v in wheels_v:
-            msg += str(v) + '\n'
+        for command in commands:
+            msg += str(command) + '\n'
 
         # print(msg)
 
         self.serial_port.write(msg.encode())
 
     def openConnection():
-        if (self.serial_port.isOpen() == False):
+        if self.serial_port.isOpen() is False:
             self.serial_port.open()
 
     def closeConnection():
-        if (self.serial_port.isOpen() == True):
+        if self.serial_port.isOpen() is True:
             self.serial_port.close()
 
     def show(self, key):
         if key.char == "q":
             print('Move')
-            self.sendCommands([0, 0, 4, 4, 0, 0])
+            self.sendData([0, 0, 4, 4, 0, 0])
         if key.char == "s":
             print('Stop')
-            mainController.sendCommands([0, 0, 0, 0, 0, 0])
+            mainController.sendData([0, 0, 0, 0, 0, 0])
 
 
 if __name__ == "__main__":
@@ -41,8 +133,14 @@ if __name__ == "__main__":
     mainController = Controller(portName)
     mainController.openConnection()
 
-    msg_start = [4.5, 0, 0, 0, 0, 0]
-    msg_stop = [0, 0, 0, 0, 0, 0]
+    config = mainController.motionPlanner(q_start, q_target)
+    wheels_v_list = mainController.wheelDrive(config[1], config[2])
+    mainController.sendCommands(wheels_v_list, config[2])
 
-    with keyboard.Listener(on_press=mainController.show) as listener:
-        listener.join()
+    mainController.closeConnection()
+
+    # msg_start = [4.5, 0, 0, 0, 0, 0]
+    # msg_stop = [0, 0, 0, 0, 0, 0]
+
+    # with keyboard.Listener(on_press=mainController.show) as listener:
+    #     listener.join()
