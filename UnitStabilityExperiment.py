b8f72e8d8c2a24c42d94dc02da6559e8f7794c67
ValenQ <85884880+ValenQiu@users.noreply.github.com>
2022-08-17 16:24:11 +0800

    Add files via upload
    LUs.py is the class.
    UnitStabilityExoeriment.py a demo, but the key.char has some problem in my computer

---
+++ UnitStabilityExperiment.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import time
+from datetime import datetime
+from pynput import keyboard
+from threading import Thread
+import pyrealsense2 as rs
+import cv2
+# import mainController
+import LUs
+
+expData = []
+
+lu = LUs.LU()
+lu.start()
+
+
+def unitsStabilityExpLoop():
+    while True:
+        # start the motion
+        centers, rotationVec = lu.DetectArucoPose()
+
+        timeStamp = datetime.now().strftime("%H:%M:%S")
+        expData.append([timeStamp, centers[2, 0], centers[2, 1],
+                        centers[1, 0], centers[1, 1]])
+        # time.sleep(0.05)
+
+
+def on_press(key):
+    if key.char == "s":
+        print('Stop')
+        lu.stop()
+        columnNames = ["time stamp", "LU1 x", "LU1 y", "LU2 x", "LU2 y"]
+        df = pd.DataFrame(expData, columns=columnNames)
+        print("save")
+        df.to_csv('unitsStabilityExp.csv')
+        return False
+
+
+if __name__ == "__main__":
+
+    # portName = "COM4"
+    # controller = mainController.Controller(portName)
+    # controller.openConnection()
+
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
+
+    Thread(target=unitsStabilityExpLoop, args=(),
+           name='unitsStabilityExpLoop', daemon=True).start()
+
+    listener.join()  # wait for abortKey
+
+    # mainController.closeConnection()
+
